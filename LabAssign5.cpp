1dd1af295ded2452da7e7b0d3ab8ac040c5378b3
GABRIEL NELLIS <5089401@nwpolytech.ca>
2024-11-15 17:10:03 -0700

    finished the lab

--- LabAssign5.cpp
+++ LabAssign5.cpp
@@ -7,47 +7,51 @@ template <class T>
 using VEC = std::vector<ELEM<T>>; //VEC = std::vector<vector<T>>
 
 template <class T>
-using action_t = T(*) (int);
+using action_t = T(*) (int); //function that takes an int and returns a variable of type T
 
 template <class T>
-using pred_t = bool (*) (T);
+using pred_t = bool (*) (T); //function that takes a variable of type T and returns a bool
 
 template <class T>
-using map_t = T(*) (T, T);
+using map_t = T(*) (T, T); //function that takes 2 variables of type T (same type) and returns a variable of type T (same type as the variables)
+
+template <class T>
+void initVec(VEC<T>& v, ELEM<T>&& cons) { //initializes the vector with the given elements
+    for (auto e : cons) {
+        ELEM<T> tmp;
+        tmp.push_back(e);
+        v.push_back(tmp);
+    }
+}
 
 template <class T>
 void printElem(ELEM<T>& v) { //prints an element of that type
     if (v.size() > 1) std::cout << "(";
     for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i] << " ";
+        if ((i < v.size()-1)) std::cout << v[i] << " , ";
+        else std::cout << v[i] << " "; //using the if else statement in order to prevent extra commas from printing
     }
     if (v.size() > 1) std::cout << ")";
 }
 
 template <class T>
-void initElem(ELEM<T>& cons, T elem) {
-    cons.push_back(elem);
-}
-
-template <class T>
-void initVec(VEC<T>& v, ELEM<T>&& cons) { //initializes the vector with the given elements
-    for (int i = 0; i < v.size(); i++) {
-        initElem(cons, cons[i]); //compiler said there was no overloaded constructer in the vector function, so I made initElem as a compromise
-    }
-}
-
-template <class T>
 void printVec(VEC<T>& v) { //prints a vector, calls printElem
+    std::cout << "[ ";
     for (int i = 0; i < v.size(); i++) {
-        printElem(v[i]); 
+        if (i < v.size() - 1) {
+            printElem(v[i]);
+            std::cout << ", "; //using the if else statement to prevent extra commas from printing, either way it'll call printElem
+        }
+        else printElem(v[i]);
     }
+    std::cout << "] \n";
 }
 
 template <class T>
 VEC<T> generate(int N, action_t<T> f) { //takes an int and a function, runs a loop to run that function (using the number in the loop) n times. produces a vector of type T
     VEC<T> tmp;
     for (int i = 0; i < N; i++) {
-        tmp.push_back(f(i));
+        tmp.push_back(ELEM<T>({f(i)})); //making f(i) an ELEM<T> to push onto tmp
     }
     return tmp;
 }
@@ -55,9 +59,13 @@ VEC<T> generate(int N, action_t<T> f) { //takes an int and a function, runs a lo
 template<class T>
 VEC<T> zip(VEC<T>& v, VEC<T>& w) { //takes 2 vectors, zips the elements together into one vector ((1),(2),(3)) ((4),(5),(6)) -> ((1,4),(2,5),(3,6))
     VEC<T> tmp;
-    for (int i = 0; i < v.size(); i++) {
-        tmp[i].push_back(v[0][i]); 
-        tmp[i].push_back(w[0][i]); 
+    for (auto e : v) { //loads tmp with the first elements (gives it a size
+        tmp.push_back(e);
+    }
+    for (int i = 0; i < tmp.size(); i++) { //since tmp is now loaded with values and therefore has a size to work with
+        for (int j = 0; j < w[i].size(); j++) { //to get the element(s) of w at the loaction which I want to push onto tmp
+            tmp[i].push_back(w[i][j]); 
+        }
     }
     return tmp;
 }
@@ -65,11 +73,11 @@ VEC<T> zip(VEC<T>& v, VEC<T>& w) { //takes 2 vectors, zips the elements together
 template<class T>
 VEC<T> filter(VEC<T>& v, pred_t<T> f) { //extracts the elements of a vector that meets the condition, tosses out those that don't
     VEC<T> tmp;
-    int counter = 0;
-    for (int i = 0; i < v.size(); i++) {
-        if (f(i)) {
-            tmp[counter].push_back(i);
-            counter++;
+    for (ELEM<T> e : v) { //goes through the elements of v (using ELEM<T> e to use e.size() for the next loop)
+        for (int i = 0; i < e.size(); i++) { //goes through the elements of e (since e is a vector of type T)
+            if (f(e[i])) { //e[i] since this'll go through all elements of e
+                tmp.push_back(e); //push_back(e) since e is a vector that can be pushed back onto tmp
+            }
         }
     }
     return tmp;
@@ -78,8 +86,10 @@ VEC<T> filter(VEC<T>& v, pred_t<T> f) { //extracts the elements of a vector that
 template<class T>
 VEC<T> map(VEC<T>& v, action_t<T> f) { //
     VEC<int> tmp;
-    for (int i = 0; i < v.size(); i++) {
-        tmp[i].push_back(f(i));  
+    for (ELEM<T> e : v) { //goes through the elements of v (using ELEM<T> e to use e.size() for the next loop)
+        for (int i = 0; i < e.size(); i++) { //goes through the elements of e (since e is a vector of type T)
+            tmp.push_back(ELEM<T>{f(e[i])}); //pushing back the result of the function as an ELEM<T>
+        }
     }
     return tmp;
 }
@@ -87,14 +97,15 @@ VEC<T> map(VEC<T>& v, action_t<T> f) { //
 template<class T>
 ELEM<T> reduce(VEC<T>& v, map_t<T> f, ELEM<T> ident) { //reduces a vector to a scalar (f(f(ident,v1),v2)) where ident shouldn't change v1 (like multiplying by 1)
     ELEM<T> sum = ident;
-    for (int i = 0; i < v.size(); i++) { 
-        sum.push_back(f(sum[0], v[0][i])); //v[0][i] since v should essentially be a vector instead of a vector of vectors
-    } 
+    for (ELEM<T> e : v) { //making e of type ELEM<T> to use e.size() since it'll be a vector
+        for (int i = 0; i < e.size(); i++) { //goes through the elements of e (needed to make e of tpye ELEM<T> to do this)
+            sum[i] = f(sum[i], e[i]); 
+        }
+    }
     return sum;
 }
 
-template<class T>
-T f(T a) {
+int f(int a) {
     return a * a;
 }
 
@@ -105,10 +116,6 @@ bool g(T a) {
 
 template<class T>
 int h(T a) {
-    /*
-    if (a > 1) return 1;
-    else return 0;
-    */
     return (a > 1 ? 1 : 0); //returns 1 if a>1, otherwise returns 0
 }
 
@@ -133,15 +140,15 @@ int main()
     VEC<int> x = zip(z, z);
     printVec(x);
     std::cout << std::string(10, '*') << std::endl;
-    //VEC<int> a = generate(10, f); //f is a function that squares
-    //printVec(a);
+    VEC<int> a = generate(10, f); //f is a function that squares   f is an unknown type?
+    printVec(a);
     VEC<int> y = filter(w, g); //g is a bool that returns true if arg > 0
     printVec(y);
     VEC<int> u = map(w, h); //h is like g but returns 1 or 0
     printVec(u);
     ELEM<int> e = reduce(u, k, ELEM<int>{0});
-    printElem(e);
-
+    printElem(e); 
+    
     std::cout << std::string(10, '$') << std::endl;
     VEC<std::string> ws;
     initVec(ws, ELEM<std::string>{"hello", "there", "franco", "carlacci"});
@@ -156,6 +163,6 @@ int main()
     ELEM<char> ec = reduce(wc, k, ELEM<char>{' '});
     std::cout << std::endl << std::string(10, '$') << std::endl;
     printElem(ec);
-
+    
     return 0;
 }
